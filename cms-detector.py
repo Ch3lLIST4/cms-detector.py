4bd76938bcad4a912864805372472ecebaa27167
Ch3lLIST4 <herohoang74@gmail.com>
2020-11-12 16:25:38 +0700

    Update cms-detector.py

--- cms-detector.py
+++ cms-detector.py
@@ -57,7 +57,7 @@ def scan():
 
     # Check to see if the site is online
     print()
-    print("[+] Checking to see if the site is online...")
+    print(G + "[+]" + C + " Checking to see if the site is online..." + W)
 
     try:
         onlineCheck = get(websiteToScan)
@@ -69,67 +69,67 @@ def scan():
             print()
             print("Beginning scan...")
             print()
-            print("[+] Checking to see if the site is redirecting...")
+            print(G + "[+]" + C + " Checking to see if the site is redirecting..." + W)
             redirectCheck = requests.get(websiteToScan, headers=user_agent)
             if len(redirectCheck.history) > 0:
                 if onlineCheck.status_code >= 300:
-                    print("[!] The site entered appears to be redirecting, please verify the destination site to ensure accurate results!")
-                    print("[!] It appears the site is redirecting to " + redirectCheck.url)
+                    print(R + "[!] The site entered appears to be redirecting, please verify the destination site to ensure accurate results!" + W)
+                    print(R + "[!] It appears the site is redirecting to " + redirectCheck.url + W)
             elif 'meta http-equiv="REFRESH"' in redirectCheck.text:
-                print("[!] The site entered appears to be redirecting, please verify the destination site to ensure accurate results!")
+                print(R + "[!] The site entered appears to be redirecting, please verify the destination site to ensure accurate results!" + W)
             else:
                 print(" | Site does not appear to be redirecting...")
         else:
-            print("[!] " + websiteToScan + " appears to be online but returned a " + str(
-                onlineCheck.status_code) + " error.")
+            print(R + "[!] " + websiteToScan + " appears to be online but returned a " + str(
+                onlineCheck.status_code) + " error." + W)
             print
             exit()
 
         print
-        print("[+] Attempting to get the HTTP headers...")
+        print(G + "[+]" + C + " Attempting to get the HTTP headers..." + W)
         # Pretty print the headers - courtesy of Jimmy
         for header in onlineCheck.headers:
             try:
                 print(" | " + header + " : " + onlineCheck.headers[header])
             except Exception as ex:
-                print("[!] Error: " + ex.message)
+                print(R + "[!] Error: " + C + str(ex) + W)
 
         ####################################################
         # WordPress Scans
         ####################################################
 
         print()
-        print("[+] Running the WordPress scans...")
+        print(G + "[+]" + C + " Running the WordPress scans..." + W)
 
         # Use requests.get allowing redirects otherwise will always fail
         wpLoginCheck = requests.get(websiteToScan + '/wp-login.php', headers=user_agent)
         if wpLoginCheck.status_code == 200 and "user_login" in wpLoginCheck.text and "404" not in wpLoginCheck.text:
-            print("[!] Detected: WordPress WP-Login page: " + websiteToScan + '/wp-login.php')
+            print(R + "[!] Detected: WordPress WP-Login page: " + websiteToScan + '/wp-login.php' + W)
         else:
             print(" |  Not Detected: WordPress WP-Login page: " + websiteToScan + '/wp-login.php')
 
         # Use requests.get allowing redirects otherwise will always fail
         wpAdminCheck = requests.get(websiteToScan + '/wp-admin', headers=user_agent)
         if wpAdminCheck.status_code == 200 and "user_login" in wpAdminCheck.text and "404" not in wpLoginCheck.text:
-            print("[!] Detected: WordPress WP-Admin page: " + websiteToScan + '/wp-admin')
+            print(R + "[!] Detected: WordPress WP-Admin page: " + websiteToScan + '/wp-admin' + W)
         else:
             print(" |  Not Detected: WordPress WP-Admin page: " + websiteToScan + '/wp-admin')
 
         wpAdminUpgradeCheck = get(websiteToScan + '/wp-admin/upgrade.php')
         if wpAdminUpgradeCheck.status_code == 200 and "404" not in wpAdminUpgradeCheck.text:
-            print("[!] Detected: WordPress WP-Admin/upgrade.php page: " + websiteToScan + '/wp-admin/upgrade.php')
+            print(R + "[!] Detected: WordPress WP-Admin/upgrade.php page: " + websiteToScan + '/wp-admin/upgrade.php' + W)
         else:
             print(" |  Not Detected: WordPress WP-Admin/upgrade.php page: " + websiteToScan + '/wp-admin/upgrade.php')
 
         wpAdminReadMeCheck = get(websiteToScan + '/readme.html')
         if wpAdminReadMeCheck.status_code == 200 and "404" not in wpAdminReadMeCheck.text:
-            print("[!] Detected: WordPress Readme.html: " + websiteToScan + '/readme.html')
+            print(R + "[!] Detected: WordPress Readme.html: " + websiteToScan + '/readme.html' + W)
         else:
             print(" |  Not Detected: WordPress Readme.html: " + websiteToScan + '/readme.html')
 
         wpLinksCheck = get(websiteToScan)
         if 'wp-' in wpLinksCheck.text:
-            print("[!] Detected: WordPress wp- style links detected on index")
+            print(R + "[!] Detected: WordPress wp- style links detected on index" + W)
         else:
             print(" |  Not Detected: WordPress wp- style links detected on index")
 
@@ -138,35 +138,35 @@ def scan():
         ####################################################
 
         print()
-        print("[+] Running the Joomla scans...")
+        print(G + "[+]" + C + " Running the Joomla scans..." + W)
 
         joomlaAdminCheck = get(websiteToScan + '/administrator/')
         if joomlaAdminCheck.status_code == 200 and "mod-login-username" in joomlaAdminCheck.text and "404" not in joomlaAdminCheck.text:
-            print("[!] Detected: Potential Joomla administrator login page: " + websiteToScan + '/administrator/')
+            print(R + "[!] Detected: Potential Joomla administrator login page: " + websiteToScan + '/administrator/' + W)
         else:
             print(" |  Not Detected: Joomla administrator login page: " + websiteToScan + '/administrator/')
 
         joomlaReadMeCheck = get(websiteToScan + '/readme.txt')
         if joomlaReadMeCheck.status_code == 200 and "joomla" in joomlaReadMeCheck.text and "404" not in joomlaReadMeCheck.text:
-            print("[!] Detected: Joomla Readme.txt: " + websiteToScan + '/readme.txt')
+            print(R + "[!] Detected: Joomla Readme.txt: " + websiteToScan + '/readme.txt' + W)
         else:
             print(" |  Not Detected: Joomla Readme.txt: " + websiteToScan + '/readme.txt')
 
         joomlaTagCheck = get(websiteToScan)
         if joomlaTagCheck.status_code == 200 and 'name="generator" content="Joomla' in joomlaTagCheck.text and "404" not in joomlaTagCheck.text:
-            print("[!] Detected: Generated by Joomla tag on index")
+            print(R + "[!] Detected: Generated by Joomla tag on index" + W)
         else:
             print(" |  Not Detected: Generated by Joomla tag on index")
 
         joomlaStringCheck = get(websiteToScan)
         if joomlaStringCheck.status_code == 200 and "joomla" in joomlaStringCheck.text and "404" not in joomlaStringCheck.text:
-            print("[!] Detected: Joomla strings on index")
+            print(R + "[!] Detected: Joomla strings on index" + W)
         else:
             print(" |  Not Detected: Joomla strings on index")
 
         joomlaDirCheck = get(websiteToScan + '/media/com_joomlaupdate/')
         if joomlaDirCheck.status_code == 403 and "404" not in joomlaDirCheck.text:
-            print("[!] Detected: Joomla media/com_joomlaupdate directories: " + websiteToScan + '/media/com_joomlaupdate/')
+            print(R + "[!] Detected: Joomla media/com_joomlaupdate directories: " + websiteToScan + '/media/com_joomlaupdate/' + W)
         else:
             print(" |  Not Detected: Joomla media/com_joomlaupdate directories: " + websiteToScan + '/media/com_joomlaupdate/')
 
@@ -175,29 +175,29 @@ def scan():
         ####################################################
 
         print()
-        print("[+] Running the Magento scans...")
+        print(G + "[+]" + C + " Running the Magento scans..." + W)
 
         magentoAdminCheck = get(websiteToScan + '/index.php/admin/')
         if magentoAdminCheck.status_code == 200 and 'login' in magentoAdminCheck.text and "404" not in magentoAdminCheck.text:
-            print("[!] Detected: Potential Magento administrator login page: " + websiteToScan + '/index.php/admin')
+            print(R + "[!] Detected: Potential Magento administrator login page: " + websiteToScan + '/index.php/admin' + W)
         else:
             print(" |  Not Detected: Magento administrator login page: " + websiteToScan + '/index.php/admin')
 
         magentoRelNotesCheck = get(websiteToScan + '/RELEASE_NOTES.txt')
         if magentoRelNotesCheck.status_code == 200 and 'magento' in magentoRelNotesCheck.text:
-            print("[!] Detected: Magento Release_Notes.txt: " + websiteToScan + '/RELEASE_NOTES.txt')
+            print(R + "[!] Detected: Magento Release_Notes.txt: " + websiteToScan + '/RELEASE_NOTES.txt' + W)
         else:
             print(" |  Not Detected: Magento Release_Notes.txt: " + websiteToScan + '/RELEASE_NOTES.txt')
 
         magentoCookieCheck = get(websiteToScan + '/js/mage/cookies.js')
         if magentoCookieCheck.status_code == 200 and "404" not in magentoCookieCheck.text:
-            print("[!] Detected: Magento cookies.js: " + websiteToScan + '/js/mage/cookies.js')
+            print(R + "[!] Detected: Magento cookies.js: " + websiteToScan + '/js/mage/cookies.js' + W)
         else:
             print(" |  Not Detected: Magento cookies.js: " + websiteToScan + '/js/mage/cookies.js')
 
         magStringCheck = get(websiteToScan + '/index.php')
         if magStringCheck.status_code == 200 and '/mage/' in magStringCheck.text or 'magento' in magStringCheck.text:
-            print("[!] Detected: Magento strings on index")
+            print(R + "[!] Detected: Magento strings on index" + W)
         else:
             print(" |  Not Detected: Magento strings on index")
 
@@ -205,13 +205,13 @@ def scan():
 
         magentoStylesCSSCheck = get(websiteToScan + '/skin/frontend/default/default/css/styles.css')
         if magentoStylesCSSCheck.status_code == 200 and "404" not in magentoStylesCSSCheck.text:
-            print("[!] Detected: Magento styles.css: " + websiteToScan + '/skin/frontend/default/default/css/styles.css')
+            print(R + "[!] Detected: Magento styles.css: " + websiteToScan + '/skin/frontend/default/default/css/styles.css' + W)
         else:
             print(" |  Not Detected: Magento styles.css: " + websiteToScan + '/skin/frontend/default/default/css/styles.css')
 
         mag404Check = get(websiteToScan + '/errors/design.xml')
         if mag404Check.status_code == 200 and "magento" in mag404Check.text:
-            print("[!] Detected: Magento error page design.xml: " + websiteToScan + '/errors/design.xml')
+            print(R + "[!] Detected: Magento error page design.xml: " + websiteToScan + '/errors/design.xml' + W)
         else:
             print(" |  Not Detected: Magento error page design.xml: " + websiteToScan + '/errors/design.xml')
 
@@ -220,35 +220,35 @@ def scan():
         ####################################################
 
         print()
-        print("[+] Running the Drupal scans...")
+        print(G + "[+]" + C + " Running the Drupal scans..." + W)
 
         drupalReadMeCheck = get(websiteToScan + '/readme.txt')
         if drupalReadMeCheck.status_code == 200 and 'drupal' in drupalReadMeCheck.text and '404' not in drupalReadMeCheck.text:
-            print("[!] Detected: Drupal Readme.txt: " + websiteToScan + '/readme.txt')
+            print(R + "[!] Detected: Drupal Readme.txt: " + websiteToScan + '/readme.txt' + W)
         else:
             print(" |  Not Detected: Drupal Readme.txt: " + websiteToScan + '/readme.txt')
 
         drupalTagCheck = get(websiteToScan)
         if drupalTagCheck.status_code == 200 and 'name="Generator" content="Drupal' in drupalTagCheck.text:
-            print("[!] Detected: Generated by Drupal tag on index")
+            print(R + "[!] Detected: Generated by Drupal tag on index" + W)
         else:
             print(" |  Not Detected: Generated by Drupal tag on index")
 
         drupalCopyrightCheck = get(websiteToScan + '/core/COPYRIGHT.txt')
         if drupalCopyrightCheck.status_code == 200 and 'Drupal' in drupalCopyrightCheck.text and '404' not in drupalCopyrightCheck.text:
-            print("[!] Detected: Drupal COPYRIGHT.txt: " + websiteToScan + '/core/COPYRIGHT.txt')
+            print(R + "[!] Detected: Drupal COPYRIGHT.txt: " + websiteToScan + '/core/COPYRIGHT.txt' + W)
         else:
             print(" |  Not Detected: Drupal COPYRIGHT.txt: " + websiteToScan + '/core/COPYRIGHT.txt')
 
         drupalReadme2Check = get(websiteToScan + '/modules/README.txt')
         if drupalReadme2Check.status_code == 200 and 'drupal' in drupalReadme2Check.text and '404' not in drupalReadme2Check.text:
-            print("[!] Detected: Drupal modules/README.txt: " + websiteToScan + '/modules/README.txt')
+            print(R + "[!] Detected: Drupal modules/README.txt: " + websiteToScan + '/modules/README.txt' + W)
         else:
             print(" |  Not Detected: Drupal modules/README.txt: " + websiteToScan + '/modules/README.txt')
 
         drupalStringCheck = get(websiteToScan)
         if drupalStringCheck.status_code == 200 and 'drupal' in drupalStringCheck.text:
-            print("[!] Detected: Drupal strings on index")
+            print(R + "[!] Detected: Drupal strings on index" + W)
         else:
             print(" |  Not Detected: Drupal strings on index")
 
@@ -257,23 +257,23 @@ def scan():
         ####################################################
 
         print()
-        print("[+] Running the phpMyAdmin scans...")
+        print(G + "[+]" + C + " Running the phpMyAdmin scans..." + W)
 
         phpMyAdminCheck = get(websiteToScan)
         if phpMyAdminCheck.status_code == 200 and 'phpmyadmin' in phpMyAdminCheck.text:
-            print("[!] Detected: phpMyAdmin index page")
+            print(R + "[!] Detected: phpMyAdmin index page" + W)
         else:
             print(" |  Not Detected: phpMyAdmin index page")
 
         pmaCheck = get(websiteToScan)
         if pmaCheck.status_code == 200 and 'pmahomme' in pmaCheck.text or 'pma_' in pmaCheck.text:
-            print("[!] Detected: phpMyAdmin pmahomme and pma_ style links on index page")
+            print(R + "[!] Detected: phpMyAdmin pmahomme and pma_ style links on index page" + W)
         else:
             print(" |  Not Detected: phpMyAdmin pmahomme and pma_ style links on index page")
 
         phpMyAdminConfigCheck = get(websiteToScan + '/config.inc.php')
         if phpMyAdminConfigCheck.status_code == 200 and '404' not in phpMyAdminConfigCheck.text:
-            print("[!] Detected: phpMyAdmin configuration file: " + websiteToScan + '/config.inc.php')
+            print(R + "[!] Detected: phpMyAdmin configuration file: " + websiteToScan + '/config.inc.php' + W)
         else:
             print(" |  Not Detected: phpMyAdmin configuration file: " + websiteToScan + '/config.inc.php')
 
